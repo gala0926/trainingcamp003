149ff14a0765dd44a545d7e6e99ac8ad76bc4548
gala0926 <547079134@qq.com>
2025-10-31 14:37:28 +0800

    Update Code06_MakeNo.java

--- src/class01/Code06_MakeNo.java
+++ src/class01/Code06_MakeNo.java
@@ -1,3 +1,6 @@
+/**
+给定一个正整数M，请构造一个长度为M的数组arr，要求对任意的i，j，k三个位置，如果i < j < k，都有arr[i] + arr[k] != 2* arr[j]，返回构造的arr
+**/
 package class01;
 
 public class Code06_MakeNo {
@@ -63,3 +66,4 @@ public class Code06_MakeNo {
 	}
 
 }
+
